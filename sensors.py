5d3e75096bc224f5ff6266b43227273118ef41a6
Chris <directive0@gmail.com>
2020-11-17 22:56:10 -0500

    Sensehat Changes

--- sensors.py
+++ sensors.py
@@ -226,21 +226,21 @@ class Sensor(object):
 
 		if configure.sensehat:# and not configure.simulate:
 
-	        if configure.moire:
-	            for x in range(8):
-	                for y in range(8):
+			if configure.moire:
+				for x in range(8):
+					for y in range(8):
 						# it's this cool plasma effect from demoscene I stole from
 						# somewhere.
-	                    cx = x + 0.5*math.sin(self.ticks/5.0)
-	                    cy = y + 0.5*math.cos(self.ticks/3.0)
-	                    v = math.sin(math.sqrt(1.0*(math.pow(cx, 2.0)+math.pow(cy, 2.0))+1.0)+self.ticks)
-	              		#v = v + math.sin(x*10.0+self.ticks)
-	                    v = (v + 1.0)/2.0
-	                    v = int(v*255.0)
-	                    sense.set_pixel(x,y,v,v,v)
-	            self.ticks = self.ticks+1
-	        else:
-	            clearled()
+						cx = x + 0.5*math.sin(self.ticks/5.0)
+						cy = y + 0.5*math.cos(self.ticks/3.0)
+						v = math.sin(math.sqrt(1.0*(math.pow(cx, 2.0)+math.pow(cy, 2.0))+1.0)+self.ticks)
+						#v = v + math.sin(x*10.0+self.ticks)
+						v = (v + 1.0)/2.0
+						v = int(v*255.0)
+						sense.set_pixel(x,y,v,v,v)
+				self.ticks = self.ticks+1
+			else:
+				clearled()
 
 			sense_data = [sense.get_temperature()]
 			sense_data2 = [sense.get_pressure()]
@@ -330,55 +330,55 @@ class Sensor(object):
 
 class MLX90614():
 
-    MLX90614_RAWIR1=0x04
-    MLX90614_RAWIR2=0x05
-    MLX90614_TA=0x06
-    MLX90614_TOBJ1=0x07
-    MLX90614_TOBJ2=0x08
+	MLX90614_RAWIR1=0x04
+	MLX90614_RAWIR2=0x05
+	MLX90614_TA=0x06
+	MLX90614_TOBJ1=0x07
+	MLX90614_TOBJ2=0x08
 
-    MLX90614_TOMAX=0x20
-    MLX90614_TOMIN=0x21
-    MLX90614_PWMCTRL=0x22
-    MLX90614_TARANGE=0x23
-    MLX90614_EMISS=0x24
-    MLX90614_CONFIG=0x25
-    MLX90614_ADDR=0x0E
-    MLX90614_ID1=0x3C
-    MLX90614_ID2=0x3D
-    MLX90614_ID3=0x3E
-    MLX90614_ID4=0x3F
+	MLX90614_TOMAX=0x20
+	MLX90614_TOMIN=0x21
+	MLX90614_PWMCTRL=0x22
+	MLX90614_TARANGE=0x23
+	MLX90614_EMISS=0x24
+	MLX90614_CONFIG=0x25
+	MLX90614_ADDR=0x0E
+	MLX90614_ID1=0x3C
+	MLX90614_ID2=0x3D
+	MLX90614_ID3=0x3E
+	MLX90614_ID4=0x3F
 
-    comm_retries = 5
-    comm_sleep_amount = 0.1
+	comm_retries = 5
+	comm_sleep_amount = 0.1
 
-    def __init__(self, address=0x5a, bus_num=1):
-        self.bus_num = bus_num
-        self.address = address
-        self.bus = smbus.SMBus(bus=bus_num)
+	def __init__(self, address=0x5a, bus_num=1):
+		self.bus_num = bus_num
+		self.address = address
+		self.bus = smbus.SMBus(bus=bus_num)
 
-    def read_reg(self, reg_addr):
-        err = None
-        for i in range(self.comm_retries):
-            try:
-                return self.bus.read_word_data(self.address, reg_addr)
-            except IOError as e:
-                err = e
-                #"Rate limiting" - sleeping to prevent problems with sensor
-                #when requesting data too quickly
-                sleep(self.comm_sleep_amount)
-        #By this time, we made a couple requests and the sensor didn't respond
-        #(judging by the fact we haven't returned from this function yet)
-        #So let's just re-raise the last IOError we got
-        raise err
+	def read_reg(self, reg_addr):
+		err = None
+		for i in range(self.comm_retries):
+			try:
+				return self.bus.read_word_data(self.address, reg_addr)
+			except IOError as e:
+				err = e
+				#"Rate limiting" - sleeping to prevent problems with sensor
+				#when requesting data too quickly
+				sleep(self.comm_sleep_amount)
+		#By this time, we made a couple requests and the sensor didn't respond
+		#(judging by the fact we haven't returned from this function yet)
+		#So let's just re-raise the last IOError we got
+		raise err
 
-    def data_to_temp(self, data):
-        temp = (data*0.02) - 273.15
-        return temp
+	def data_to_temp(self, data):
+		temp = (data*0.02) - 273.15
+		return temp
 
-    def get_amb_temp(self):
-        data = self.read_reg(self.MLX90614_TA)
-        return self.data_to_temp(data)
+	def get_amb_temp(self):
+		data = self.read_reg(self.MLX90614_TA)
+		return self.data_to_temp(data)
 
-    def get_obj_temp(self):
-        data = self.read_reg(self.MLX90614_TOBJ1)
-        return self.data_to_temp(data)
+	def get_obj_temp(self):
+		data = self.read_reg(self.MLX90614_TOBJ1)
+		return self.data_to_temp(data)
