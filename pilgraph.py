44b5a31e95389a3c3ae0bf14b0c20fbaf4e71d38
Chris <directive0@gmail.com>
2021-04-04 03:28:41 -0400

    plarstest

--- pilgraph.py
+++ pilgraph.py
@@ -166,6 +166,7 @@ class graph_area(object):
 		self.auto = configure.auto[0]
 
 		dsc = configure.sensor_info[configure.sensors[self.ident][0]][3]
+		dev = configure.sensor_info[configure.sensors[self.ident][0]][5]
 
 		#preps the list by adding the X coordinate to every sensor value
 		cords = self.graphprep(plars.get_recent(dsc,dev,num = self.spanx))
