7f8c6391ca90a57f8de3095e5bcf385b014f410f
Chris <directive0@gmail.com>
2021-08-13 12:10:07 -0400

    threading

--- leds.py
+++ leds.py
@@ -220,11 +220,12 @@ class ripple(object):
 def ripple_async(in_q):
 	thread_rip = ripple()
 
-	print(timer.timelapsed())
-	print(interval)
-	if timer.timelapsed() > interval:
-		thread_rip.cycle()
-		timer.logtime()
+	while true:
+		print(timer.timelapsed())
+		print(interval)
+		if timer.timelapsed() > interval:
+			thread_rip.cycle()
+			timer.logtime()
 
 
 	pass
