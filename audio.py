0348bc3f18e037b87bd2e8d991631f08cd71bc51
Chris <directive0@gmail.com>
2021-08-20 14:37:12 -0400

    thread

--- audio.py
+++ audio.py
@@ -16,14 +16,14 @@ def threaded_audio():
     timed = timer()
     start = True
     was_open = False
-    warble = sa.play_buffer(scansounds, 2, 2, 44100)
+    warble = sa.play_buffer(scansound, 2, 2, 44100)
 
     while not configure.status[0] == "quit":
         if configure.audio[0]:
 
             if configure.dr_open[0]:
                 if not warble.is_playing():
-                    warble = sa.play_buffer(scansounds, 2, 2, 44100)
+                    warble = sa.play_buffer(scansound, 2, 2, 44100)
             else:
                 if warble.is_playing():
                     warble.stop()
