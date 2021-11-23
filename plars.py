4c93fb24d37acc3ba395cf0dc0fa31ef33a56e4d
Chris <directive0@gmail.com>
2021-11-23 14:26:22 -0500

    plars fixes

--- plars.py
+++ plars.py
@@ -128,15 +128,15 @@ class PLARS(object):
 		print("Updating EM Dataframe:")
 		print(data)
 		newdata = pd.DataFrame(columns=['ssid','signal','quality','frequency','encrypted','channel','dev','mode','dsc','timestamp'])
+		newdata.reset_index(drop=True)
 
 		# sets/requests the thread lock to prevent other threads reading data.
 		self.lock.acquire()
 
-		print("got lock")
+
 		# appends the new data to the buffer
-		print("appending")
 		self.buffer_em = self.buffer_em.append(newdata, ignore_index=True)
-		print("appended")
+
 
 		self.lock.release()
 		print("released lock")
