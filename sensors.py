b8766b3d39c39058520373d18115beb50ffe61d7
chris <directive0@gmail.com>
2022-01-12 18:40:22 -0500

    pocketgeiger fix

--- sensors.py
+++ sensors.py
@@ -144,7 +144,7 @@ class Sensor(object):
 			self.VOC_info = [300000,1100000,"VOC","KOhm", "BME680"]
 
 		if configure.pocket_geiger:
-			self.radiation_info = [0.05,10000.0,"Radiation","uSv/h", "PocketGeiger"]
+			self.radiation_info = [0.05,10000.0,"Radiation","usvh", "PocketGeiger"]
 			self.radiation = RadiationWatch(configure.PG_SIG,configure.PG_NS)
 			self.radiation.setup()
 
