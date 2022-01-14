71ed7e96ba0b8e39e437cf0a4a5fa5fc44b5d79c
Chris <directive0@gmail.com>
2022-01-13 22:57:04 -0500

    tests

--- tos_display.py
+++ tos_display.py
@@ -588,7 +588,8 @@ class Graph_Screen(object):
 			b_newest = float(senseslice[1][0])
 			c_newest = float(senseslice[2][0])
 			newests = [a_newest,b_newest,c_newest]
-
+		except:
+			return status
 
 		# updates the data storage object and retrieves a fresh graph ready to store the positions of each segment for the line drawing
 		a_cords = graphit(self.data_a,senseslice[0])
