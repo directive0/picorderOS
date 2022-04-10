11288c1796b9eb7d6a00682d838ac455b737ff04
Chris <directive0@gmail.com>
2022-04-10 01:04:30 -0400

    TFT24 fix

--- lcars_clr.py
+++ lcars_clr.py
@@ -1070,9 +1070,6 @@ class ColourScreen(object):
 
 	def __init__(self):
 
-		if configure.display == 2:
-			self.surface = device.draw()
-
 		# instantiates an image and uses it in a draw object.
 		self.image = Image.open('assets/lcarsframe.png')
 		self.blankimage = Image.open('assets/lcarsframeblank.png')
