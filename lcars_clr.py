718807396bc461789bb2c132540708ee84e4f3cb
Chris <directive0@gmail.com>
2023-05-16 01:06:59 -0400

    UI changes

--- lcars_clr.py
+++ lcars_clr.py
@@ -926,7 +926,7 @@ class MultiFrame(object):
 
 		# Sets the x and y span of the graph
 		self.gspanx = 133
-		self.gspany = 69
+		self.gspany = 68
 
 		self.graphcycle = 0
 
@@ -1030,15 +1030,15 @@ class MultiFrame(object):
 		if self.selection == 0:
 			raw_a = str(self.A_Data)
 			adjusted_a = self.arrangelabel(raw_a)
-			a_string = adjusted_a + " " + configure.sensor_info[configure.sensor1[0]][2]
+			a_string = adjusted_a + " " + str(configure.sensor_info[configure.sensor1[0]][2])[:6]
 
 			raw_b = str(self.B_Data)
 			adjusted_b = self.arrangelabel(raw_b)
-			b_string = adjusted_b + " " + configure.sensor_info[configure.sensor2[0]][2]
+			b_string = adjusted_b + " " + str(configure.sensor_info[configure.sensor2[0]][2])[:6]
 
 			raw_c = str(self.C_Data)
 			adjusted_c = self.arrangelabel(raw_c)
-			c_string = adjusted_c + " " + configure.sensor_info[configure.sensor3[0]][2]
+			c_string = adjusted_c + " " + str(configure.sensor_info[configure.sensor3[0]][2])[:6]
 
 			self.A_Label.string = a_string
 			self.A_Label.push(self.labelx,self.labely,self.draw)
